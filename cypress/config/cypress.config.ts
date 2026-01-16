971715a5eabffe30a417698de092f8d0e7d45bb9
abdul-qadir92 <abdulqadir.k@browserstack.com>
2026-01-16 19:40:50 +0530

    updated cli

--- cypress/config/cypress.config.ts
+++ cypress/config/cypress.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://example.cypress.io',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
+    fixturesFolder: 'cypress/fixtures',
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
+    downloadsFolder: 'cypress/downloads',
     viewportWidth: 1280,
     viewportHeight: 720,
     video: true,
     screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 })
-
