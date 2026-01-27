a2c3d4bca7394b0bb15288f650fe33524c8f8328
Aaryan <aaryan@browserstack.com>
2026-01-27 18:33:39 +0530

    fix config

--- cypress/config/cypress.config.ts
+++ cypress/config/cypress.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'https://example.cypress.io',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.ts',
-    fixturesFolder: 'cypress/fixtures',
-    videosFolder: 'cypress/videos',
-    screenshotsFolder: 'cypress/screenshots',
-    downloadsFolder: 'cypress/downloads',
+    specPattern: '../e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: '../support/e2e.ts',
+    fixturesFolder: '../fixtures',
+    videosFolder: '../videos',
+    screenshotsFolder: '../screenshots',
+    downloadsFolder: '../downloads',
     viewportWidth: 1280,
     viewportHeight: 720,
     video: true,
